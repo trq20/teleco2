cc41f6a5ff7ee91d7e957a7336df5083a920d8c9
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2021-05-31 19:57:42 -0300

    Add files via upload

---
+++ rendimiento.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+# TODO
+
+plt.grid()
+plt.xlabel('Indice de modulacion - m')
+plt.ylabel('Rendimiento -  \u019E [%]')
+plt.title('Rendimiento AM')
+plt.legend()
+plt.show()
